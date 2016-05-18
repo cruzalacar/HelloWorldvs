ebc18a3760021e4cc6c1fb2a7049d3212ce4e9f6
Owner <Owner@HOME>
2016-05-18 12:47:39 -0400

    Add project files.

---
+++ HelloConsoleWorld/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloConsoleWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ask the user for their name
+            Console.WriteLine("Hi, what is your name?");
+
+            //read the user input
+            string userInput = Console.ReadLine();
+
+            //Say hello to them
+            Console.WriteLine("Hello {0}", userInput);
+
+            //Pause and wait for the user to exit to program
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HelloWinFormsWorld/HelloWorldForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWinFormsWorld
+{
+    public partial class HelloWorldForm : Form
+    {
+        public HelloWorldForm()
+        {
+            InitializeComponent();
+        }
+
+        private void OnSayHello(object sender, EventArgs e)
+        {
+            //obtain the name of the user
+            string userName = textBox1.Text;
+
+            //say hello to the user
+            MessageBox.Show(String.Format("Hello {0}", userName), "Hello WinForm World");
+        }
+    }
+}
